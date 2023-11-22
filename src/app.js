9c96dc47b855a582847f08e80ee4139073dadfdf
Hanish Tharwani <104623869+hanishtharwani123@users.noreply.github.com>
2023-11-22 18:49:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const static_path = path.join(__dirname, "../public");
+// MongoDB Connection
+mongoose.connect("mongodb://localhost:27017/your-database-name", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(express.static(static_path));
+// Define a mongoose schema for the images
+const imageSchema = new mongoose.Schema({
+  data: Buffer,
+  contentType: String,
+});
+
+const Image = mongoose.model("Image", imageSchema);
+
+// Set up multer for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// Serve static files (images) from the 'uploads' folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Set up view engine
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -16,6 +38,21 @@ app.get("/", (req, res) => {
 
 app.get("/signup", (req, res) => {
   res.render("main");
+});
+
+// Handle file upload
+app.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    const newImage = new Image({
+      data: req.file.buffer,
+      contentType: req.file.mimetype,
+    });
+
+    await newImage.save();
+    res.send("Image uploaded successfully!");
+  } catch (error) {
+    res.status(500).send("Error uploading image");
+  }
 });
 
 app.listen(port, () => {
